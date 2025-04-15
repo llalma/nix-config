47e45490060873a62731384fd9e71ed1c23851f0
llalma <lhulsmanbenson@gmail.com>
2025-04-15 20:53:02 +1000

    Add configonly if running as wsl

--- configuration.nix
+++ configuration.nix
@@ -5,22 +5,21 @@ let
   home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz;
 in
 {
+
+  config = lib.mkIf (builtins.pathExists "/proc/sys/fs/binfmt_misc/WSLInterop") {
+    imports = [
+      <nixos-wsl/modules>
+    ];
+    wsl.enable = true;
+    wsl.defaultUser = "llalma";
+  };
+
   imports = [
-    <nixos-wsl/modules>
     (import "${home-manager}/nixos")
   ];
 
-  wsl.enable = true;
-  wsl.defaultUser = "llalma";
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-    
+  system.stateVersion = "24.11"; 
   
   # Config for personal user
   users.users.llalma.isNormalUser = true;
