488595feb1982d2414676e4ff18df2702d5f4437
llalma <lhulsmanbenson@gmail.com>
2025-04-15 22:11:08 +1000

    y

--- native.nix
+++ native.nix
@@ -17,7 +17,7 @@
   networking.wireless.enable = true;
   networking.wireless.networks = {
     "FinallyNBN" = {
-      psk = 47c966e97407a15427a240dc758e45ab2a18a6cfd1b1904800973515734fa3aa;
+      pskRaw = "47c966e97407a15427a240dc758e45ab2a18a6cfd1b1904800973515734fa3aa";
     };
   };
 
