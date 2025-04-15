b413e78f9e260d93050d2d7d6d8d5c6cb25e41bc
llalma <lhulsmanbenson@gmail.com>
2025-04-15 22:09:30 +1000

    y

--- native.nix
+++ native.nix
@@ -14,8 +14,12 @@
   };
 
   # Networking
-  networking.networkmanager.enable = true;
-  users.users.llalma.extraGroups = [ "networkmanager" ];
+  networking.wireless.enable = true;
+  networking.wireless.networks = {
+    "FinallyNBN" = {
+      psk = "47c966e97407a15427a240dc758e45ab2a18a6cfd1b1904800973515734fa3aa";
+    };
+  };
 
 
 }
